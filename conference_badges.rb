140e43319748cdedcf42734535e8e5acb6b9a4d2
Jose Chavira <joeychavira2016@gmail.com>
2019-10-11 01:45:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,14 +6,14 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(names)
-  names.map do |names|
-    "Hello, my name is #{names}."
+  names.map do |name|
+    "Hello, my name is #{name}."
   end
 end
 
 def assign_rooms(names)
-  names.each_with_index.map do |names, index|
-    "Hello, #{names}! You'll be assigned to room #{index+1}"
+  names.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}"
   end
 end
 
